dff70c5fe57c6ae0af34e006ad7138e40b67e9fc
santhoshK12 <kistipatisanthosh851@gmail.com>
2026-01-30 05:34:51 +0000

    Refactored to modular architecture: VPC and EC2 separated

---
+++ modules/vpc/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "Module-VPC" }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = { Name = "Module-Subnet" }
+}
+
+output "subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
